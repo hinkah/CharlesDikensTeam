e4e1e73c4ce4538b8e4ab145e76bc7f258bd937c
StefanMihaylov <mihaylov.s@gmail.com>
2014-02-23 19:50:14 +0200

    Added two additional classes, added exseption class, updated class diagram

---
+++ Fighters/FightersCon/InvalidPowerValueException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightersCon
+{
+    public class InvalidPowerValueException : ApplicationException
+    {
+        public InvalidPowerValueException(string msg)
+            : base(msg)
+        {
+        }
+
+        public InvalidPowerValueException(string msg, Exception innerEx)
+            : base(msg, innerEx)
+        {
+        }
+
+    }
+}
+
---
+++ Fighters/FightersCon/MovableObjects/Villager.cs
@@ -0,0 +1,21 @@
+﻿
+namespace FightersCon
+{
+    public class Villager : Character
+    {
+        public const int VillagerLife = 200;
+        public const int VillagerAttackPower = 0;
+        public const int VillagerDefencePower = 0;
+
+        public Villager(MatrixCoords topLeft, char[,] body, MatrixCoords speed,
+                int attackPower, int defencePower, int gold)
+            : base(topLeft, body, speed, VillagerAttackPower, VillagerDefencePower)
+        {
+            this.Life = VillagerLife;
+            this.Gold = gold;
+
+        }
+
+        public int Gold { get; set; }
+    }
+}
