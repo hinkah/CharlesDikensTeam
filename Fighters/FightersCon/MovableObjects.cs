19414da846973df1b56a8c099913807682bfcea4
hinkah <hinka.hineva@gmail.com>
2014-02-23 13:50:36 +0200

    changed classes

--- Fighters/FightersCon/MovableObjects.cs
+++ Fighters/FightersCon/MovableObjects.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace FightersCon
 {
     public abstract class MovableObject : WorldObject, IMovable, IControllable
